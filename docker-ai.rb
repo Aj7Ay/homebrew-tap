d73701c6c755961bc64eec3b0f79c74367a2473d
ajay <ajay@practical-devsecops.com>
2025-06-14 19:55:00 +0530

    Update docker-ai to v0.2.4

--- docker-ai.rb
+++ docker-ai.rb
@@ -1,8 +1,8 @@
 class DockerAi < Formula
   desc "An AI-powered CLI for Docker"
   homepage "https://github.com/Aj7Ay/docker-ai"
-  url "https://github.com/Aj7Ay/docker-ai/archive/refs/tags/v0.2.0.tar.gz"
-  sha256 "da67633215f9999b986b244d2d4157d07931f7956a81e050302b11562af9e27c"
+  url "https://github.com/Aj7Ay/docker-ai/archive/refs/tags/v0.2.4.tar.gz"
+  sha256 "452aa8d076a496550d87deefb6397160452940125852322c145405379e81f596"
   license "Apache-2.0"
 
   depends_on "go" => :build
